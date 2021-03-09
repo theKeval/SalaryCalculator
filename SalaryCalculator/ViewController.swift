39d72cd7eb6fab4f31a3e7ed73aeea9bc901c3d7
Keval <keval.langalia@live.com>
2021-03-09 14:13:30 +0530

    added few UI components

--- SalaryCalculator/ViewController.swift
+++ SalaryCalculator/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var qualificationPicker: UIPickerView!
+    @IBOutlet weak var btnSingle: UIButton!
+    @IBOutlet weak var btnNotSingle: UIButton!
+    @IBOutlet weak var switchKids: UISwitch!
+    
+    @IBOutlet weak var lableTemp: UILabel!
+    
+    var qualifications = ["Bachelor", "Diploma", "High School"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        qualificationPicker.delegate = self
+        qualificationPicker.dataSource = self
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return qualifications.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return qualifications[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // selected row at 'row'
+    }
+    
+    @IBAction func btnSingleClicked(_ sender: UIButton) {
+        btnSingle.isSelected = true
+        btnNotSingle.isSelected = false
+    }
+    @IBAction func btnNotSingleClicked(_ sender: UIButton) {
+        btnSingle.isSelected = false
+        btnNotSingle.isSelected = true
     }
 
-
+    @IBAction func switchKids(_ sender: UISwitch) {
+        if sender.isOn {
+            lableTemp.text = "ON"
+        }
+        else {
+            lableTemp.text = "OFF"
+        }
+    }
+    
 }
 
